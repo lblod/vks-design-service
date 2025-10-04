aef7de07284d9bdbbbf0ed6099adaa24f09b46c8
abeforgit <arnebertrand@gmail.com>
2025-10-04 07:16:09 +0200

    feat: add kurier setup

--- app.ts
+++ app.ts
@@ -1,8 +1,18 @@
 import { app } from 'mu';
+import type { Application } from 'express';
+import { Application as KApp, jsonApiExpress } from 'kurier';
+import { Design, DesignProcessor } from './lib/resources/design';
 
-export const myapp = app;
+const kurierApp = new KApp({
+  namespace: 'api',
+  types: [Design],
+  processors: [DesignProcessor],
+  defaultProcessor: DesignProcessor,
+});
+export const myapp: Application = app;
 myapp.get('/hello', function (_req, res) {
   // throw new Error('asdfasfd');
   res.status(200);
   res.send({ response: 'Hello mu-javascript-template' });
 });
+myapp.use(jsonApiExpress(kurierApp));
