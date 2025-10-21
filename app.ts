5f6349c431e2bd8d51e3f409e8446d0efe745e72
piemonkey <piemonkey@protonmail.ch>
2025-10-21 18:43:34 +0200

    Change route ordering to allow subroutes to match
    Matching is done on a 'first match' basis, so we need to define
    subroutes before routes...

--- app.ts
+++ app.ts
@@ -5,6 +5,6 @@ import { designMeasuresRouter } from './lib/routes/ar-designs/measures.ts';
 import { measureVariablesRouter } from './lib/routes/measures/variables.ts';
 
 export const myapp: Application = app;
-myapp.use(designsRouter);
 myapp.use(designMeasuresRouter);
 myapp.use(measureVariablesRouter);
+myapp.use(designsRouter);
