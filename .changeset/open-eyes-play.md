a7671e2fccd5095e1b0bdd254e321c7dc41988c8
piemonkey <piemonkey@protonmail.ch>
2025-12-29 19:06:09 +0100

    Include lists of unused and un-included signals in measure-designs

---
+++ .changeset/open-eyes-play.md
@@ -0,0 +1,5 @@
+---
+'vks-design-service': minor
+---
+
+Include details of inconsistencies between the signal concepts in designs and in measure concepts
--- lib/services/measure-designs.ts
+++ lib/services/measure-designs.ts
@@ -1,6 +1,7 @@
 import { getARDesignById } from '../db/ar-designs';
 import { getMeasureConceptByUri } from '../db/measure-concepts';
 import { getMeasureDesigns } from '../db/measure-designs';
+import { getTrafficSignalConcepts } from '../db/traffic-signal-concepts';
 import { getTrafficSignals } from '../db/traffic-signals';
 import type { ArDesign } from '../schemas/ar-design';
 import type { MeasureDesign } from '../schemas/measure-design';
@@ -29,24 +30,62 @@ const MeasureDesignsService = {
     const measureDesignsResolved: MeasureDesign[] = (
       await Promise.all(
         measureDesigns.map(async (measureDesign) => {
-          const measureConceptResolved = await getMeasureConceptByUri(
+          const measureConceptPromise = getMeasureConceptByUri(
             measureDesign.measureConcept,
           );
+          const trafficSignalsPromise = getTrafficSignals({
+            uris: measureDesign.trafficSignals as string[],
+          });
+          const [measureConceptResolved, trafficSignalsResolved] =
+            await Promise.all([measureConceptPromise, trafficSignalsPromise]);
           if (!measureConceptResolved) {
             return;
           }
-          const trafficSignalsResolved = await getTrafficSignals({
-            uris: measureDesign.trafficSignals as string[],
-          });
+
+          const usedSignalConceptUris = [
+            ...new Set(
+              trafficSignalsResolved.map(
+                (signal) => signal.trafficSignalConcept.uri,
+              ),
+            ),
+          ];
+          const measureSignalConceptUris =
+            measureConceptResolved.signalConcepts;
+          const unusedSignalConceptUris = measureSignalConceptUris.filter(
+            (included) => !usedSignalConceptUris.includes(included),
+          );
+          const unIncludedSignalConceptUris = usedSignalConceptUris.filter(
+            (used) => !measureSignalConceptUris.includes(used),
+          );
+          // We haven't got the data for signal concepts that have not been used, so fetch that
+          const unusedSignalConcepts =
+            unusedSignalConceptUris.length > 0
+              ? await getTrafficSignalConcepts({
+                  uris: unusedSignalConceptUris,
+                })
+              : [];
+          // Signal concepts that are used but not included already have data in the list of traffic
+          // signals, so we can find it there to avoid more queries
+          const unIncludedSignalConcepts = unIncludedSignalConceptUris
+            .map(
+              (uri) =>
+                trafficSignalsResolved.find(
+                  (signal) => signal.trafficSignalConcept.uri === uri,
+                )?.trafficSignalConcept,
+            )
+            .filter(isSome);
           const variableInstances =
             await VariableInstancesService.getVariableInstances({
               measureConcept: measureConceptResolved,
               measureDesign,
             });
+
           return {
             ...measureDesign,
-            measureConcept: measureConceptResolved!,
+            measureConcept: measureConceptResolved,
             trafficSignals: trafficSignalsResolved,
+            unusedSignalConcepts,
+            unIncludedSignalConcepts,
             variableInstances,
           };
         }),
